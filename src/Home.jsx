0544eea31fb78fdb680111aa4f7cb0e80669adb1
sujit-mandal <sm.bright22@gmail.com>
2024-03-06 23:13:45 +0600

    Navbar design complete

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,29 @@
 import Navbar from "./components/Navbar";
-
+import { BiRadioCircle } from "react-icons/bi";
+import { BiRadioCircleMarked } from "react-icons/bi";
 const Home = () => {
   return (
-    <div className="max-w-screen-xl mx-auto">
-      <Navbar />
-      <div className="bg-hero">
-        <h1>Hello</h1>
+    <div className="">
+      <div className="bg-hero bg-no-repeat bg-cover bg-center">
+        <Navbar />
+        <div className="max-w-screen-xl mx-auto">
+          <h3 className="text-white text-[40px] font-semibold ml-[52px] mt-12">
+            Welcome To <span className="text-[#006CE4]">AirBook</span>
+          </h3>
+          <div>
+            <div>
+              <div className="">
+                <span className="inline-flex items-center ">
+                  <BiRadioCircle /> One Way
+                </span>
+                <span>
+                  <BiRadioCircleMarked /> Round Trip
+                </span>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         // Define custom colors using RGBA
-        'blue-start': 'rgba(0, 108, 228, 1)', 
-        'blue-end': 'rgba(0, 108, 228, .8)', 
+        "blue-start": "rgba(0, 108, 228, 1)",
+        "blue-end": "rgba(0, 108, 228, .8)",
+        "nav-bg": "rgba(255, 255, 255, 0.9)",
       },
       backgroundImage: {
-        'hero': "url('./src/assets/background.png')",
-      }
+        hero: "url('./src/assets/background.png')",
+      },
     },
   },
   plugins: [],
-}
-
+};
